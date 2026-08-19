91604a3a18e7b679a323c9845e6b55ca4522dad4
SAMSAMYTFF33 <miricanmoroco@gmail.com>
2026-08-19 12:14:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -85,8 +85,8 @@ ACCOUNT_PROXIES = {
     ]
 }
 
-PROXY_USER = "nbrhdwnj"
-PROXY_PASS = "0tppl9zac913"
+PROXY_USER = "dwzxxjfl"
+PROXY_PASS = "e1in83kj5c8x"
 
 def check_single_exempt_proxy(prx):
     """دالة فرعية لفحص البروكسي الثابت وإرجاع الوقت والـ URL"""
